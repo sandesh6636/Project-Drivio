c7595fd6ae5b74f2acfd9a27201375b5e9bec4e3
Sandesh Rai <2dolist99@gmail.com>
2024-06-08 19:56:40 +0545

    fix the logn page input

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const home_btn = document.querySelector("#drivio-btn");
+
 home_btn.addEventListener("click", () => {
   window.location.href = "index.html";
 });
@@ -11,4 +12,5 @@ sign_up_btn.addEventListener("click", () => {
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+  // container.style.zIndex = "6";
 });
